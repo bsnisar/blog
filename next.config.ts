2c4d8c296f6b21cdc467168c824717459b92a384
bsnisar <bogdan.sns@gmail.com>
2026-09-16 18:53:20 +0200

    Enable Markdown table support

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
-const withMDX = require('@next/mdx')()
+
+const withMDX = require('@next/mdx')({
+  options: {
+    remarkPlugins: ["remark-gfm"],
+  },
+})
 
 const nextConfig: NextConfig = {
 	/* config options here */
